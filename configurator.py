a012cc5e8fae89cfa5f59dcf005efb792b165c47
GuyMcBride <guy.mcbride@ntlworld.com>
2021-06-09 11:45:34 +0100

    Played with different iterations and start values

--- configurator.py
+++ configurator.py
@@ -157,9 +157,9 @@ def main():
         Register("HVI_CH1_Phase0", 0),
         Register("HVI_CH3_Phase0", 0),
         Register("HVI_CH4_Phase0", 0),
-        Register("HVI_CH1_Amplitude0", int(0.25 * 0xffff)),
-        Register("HVI_CH3_Amplitude0", int(0.25 * 0xffff)),
-        Register("HVI_CH4_Amplitude0", int(0.25 * 0xffff)),
+        Register("HVI_CH1_Amplitude0", 0),
+        Register("HVI_CH3_Amplitude0", 0),
+        Register("HVI_CH4_Amplitude0", 0),
     ]
 
     # Fpga:
@@ -188,7 +188,7 @@ def main():
         Register("IterationCounter", 0),
         Register("FrequencyIterator", A(lo1_1_0)),
         Register("PhaseIterator", 0),
-        Register("AmplitudeIterator", int(0.25 * 0xffff)),
+        Register("AmplitudeIterator", int(0.25 * 0xFFFF)),
         Register("AB", 0),
     ]
 
@@ -282,10 +282,10 @@ def main():
         HviConstant("NumberOfLoops", loops),
         HviConstant("NumberOfIterations", iterations),
         HviConstant("Gap", int(pulseGap / 1e-9)),
-#        HviConstant("FrequencyIncrement", A(lo1_1_0)),
+        #        HviConstant("FrequencyIncrement", A(lo1_1_0)),
         HviConstant("FrequencyIncrement", 0),
-        HviConstant("PhaseIncrement", int(90 * 1024/360)),
-        HviConstant("AmplitudeIncrement", int(0.25 * 0xffff)),
+        HviConstant("PhaseIncrement", int(180 * 1024 / 360)),
+        HviConstant("AmplitudeIncrement", int(0.75 * 0xFFFF)),
     ]
 
     # HVI:
